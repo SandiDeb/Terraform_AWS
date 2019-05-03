5f3fb1e6c3d2887c8d80ededc1e256aabf88a08c
Deb <sandipan.deb@capgemini.com>
2019-05-03 14:30:26 +0100

    Modified ec2.tf and added keypair.tf

---
+++ keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "deep" {
+  name       = "deep-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3zZhnMTEtAI40E+pArONuObu48fHFfz7z+ICESSQUChi3RZW7d2CicHwrmmDQIT4CHZlWsSxLAgSonmPtQwZVJZpBycG2nld1yQoVH+fqdhDlzwvB1HEqNcxObG07xzWBydfpFkvPMnkpRbG2sHKHMQdtsCrsegOd8qfAu4904WiFWA9E2F/kQdx7jvXg21T17fzGmkuwLoKx9eR9WhJFqg8WjsGSsfg7YR+UuSa92qWLrYncqhDphNzd8hFfc6Jt1iQDcfbmmyDl8NxY49xRGkCUIcHgwKl6J/YdRL4Y/rZv1wgBw4elxOi9Hk7BzI94MrYsOqiLCo6OHKuagxQp dluser@TestELK"
+}
